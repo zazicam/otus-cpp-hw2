c96f4b983e74a12008e60e893106255d1b766fc7
zazicam <dima.friends@mail.ru>
2019-04-24 17:01:23 +0300

    add more tests

---
+++ ip_filter.hpp
@@ -0,0 +1,66 @@
+#ifndef IP_FILTER_HPP
+#define IP_FILTER_HPP
+
+using vec_of_strings = std::vector<std::string>;
+
+int version() { return 1; }
+
+vec_of_strings split(const std::string &str, char d)
+{
+    std::vector<std::string> r;
+    std::string::size_type start = 0;
+    std::string::size_type stop = str.find_first_of(d);
+    while(stop != std::string::npos)
+    {
+        r.push_back(str.substr(start, stop - start));
+
+        start = stop + 1;
+        stop = str.find_first_of(d, start);
+    }
+    r.push_back(str.substr(start));
+    return r;
+}
+
+bool greater(const vec_of_strings &a, const vec_of_strings &b)
+{
+	int size = std::min(a.size(), b.size());
+	for(int i=0;i<size;i++)
+	{
+		int x = std::stoi(a[i]);
+		int y = std::stoi(b[i]);
+		if(x>y) return true;
+		if(x<y) return false;
+	}
+	return false;
+}
+
+bool is_match(const vec_of_strings &ip, const std::string &mask_str)
+{
+	vec_of_strings mask = split(mask_str, '.');
+	for(int i=0;i<mask.size();i++)
+	{
+		if(mask[i]!="*" && std::stoi(ip[i]) != std::stoi(mask[i]))
+			return false;
+	}
+	return true;
+}
+
+std::vector<vec_of_strings> filter(const std::vector<vec_of_strings> &ip_pool, const std::string &mask_str)
+{
+	std::vector<vec_of_strings> res;
+	vec_of_strings masks = split(mask_str, '|');
+	for(auto ip: ip_pool)
+	{
+		for(auto mask: masks)
+		{
+			if(is_match(ip, mask)) 
+			{
+				res.push_back(ip);
+				break;
+			}
+		}
+	}
+	return res;
+}
+
+#endif // IP_FILTER_HPP
---
+++ tests/test.cpp
@@ -0,0 +1,95 @@
+#include "gtest/gtest.h"
+#include <fstream>
+
+#include "../ip_filter.hpp"
+
+using VOS = vec_of_strings;
+
+TEST(split_tests, test1)
+{
+	ASSERT_TRUE( split("", '.')      == VOS ({""}) );
+	ASSERT_TRUE( split("11", '.')    == VOS ({"11"}) );
+	ASSERT_TRUE( split("..", '.')    == VOS ({"", "", ""}) );
+	ASSERT_TRUE( split("11.", '.')   == VOS ({"11", ""}) );
+	ASSERT_TRUE( split(".11", '.')   == VOS ({"", "11"}) );
+	ASSERT_TRUE( split("11.22", '.') == VOS ({"11", "22"}) );
+}
+
+TEST(greater_tests, test1)
+{
+	ASSERT_TRUE( greater(VOS({"0","0","0","2"}), VOS({"0","0","0","1"}) )); 
+	ASSERT_TRUE( greater(VOS({"0","0","2","0"}), VOS({"0","0","1","0"}) )); 
+	ASSERT_TRUE( greater(VOS({"0","2","0","1"}), VOS({"0","1","0","0"}) )); 
+	ASSERT_TRUE( greater(VOS({"2","0","0","1"}), VOS({"1","0","0","0"}) )); 
+	ASSERT_TRUE( greater(VOS({"1","2","3","4"}), VOS({"1","2","3","3"}) )); 
+	ASSERT_TRUE( greater(VOS({"1","1","1","1"}), VOS({"0","0","0","2"}) )); 
+	ASSERT_TRUE( greater(VOS({"4","3","2","1"}), VOS({"1","2","3","4"}) )); 
+	ASSERT_TRUE( greater(VOS({"1","1","1","123"}), VOS({"1","1","1","122"}) )); 
+}
+
+TEST(greater_tests, test2)
+{
+	ASSERT_FALSE( greater(VOS({"0","0","0","0"}), VOS({"0","0","0","0"}) )); 
+	ASSERT_FALSE( greater(VOS({"0","0","0","1"}), VOS({"0","0","1","0"}) )); 
+	ASSERT_FALSE( greater(VOS({"0","0","0","1"}), VOS({"0","1","0","0"}) )); 
+	ASSERT_FALSE( greater(VOS({"0","0","0","1"}), VOS({"1","0","0","0"}) )); 
+	ASSERT_FALSE( greater(VOS({"1","2","3","4"}), VOS({"1","2","3","4"}) )); 
+	ASSERT_FALSE( greater(VOS({"1","1","1","1"}), VOS({"2","2","2","2"}) )); 
+	ASSERT_FALSE( greater(VOS({"1","1","1","01"}), VOS({"1","1","1","1"}) )); 
+	ASSERT_FALSE( greater(VOS({"1","1","1","012"}), VOS({"1","1","1","12"}) )); 
+}
+
+TEST(is_match_tests, test1)
+{
+	ASSERT_TRUE( is_match(VOS({"1", "2", "3", "4"}), "*.*.*.*") );
+	ASSERT_TRUE( is_match(VOS({"1", "2", "3", "4"}), "1.*.*.*") );
+	ASSERT_TRUE( is_match(VOS({"1", "2", "3", "4"}), "1.2.*.*") );
+	ASSERT_TRUE( is_match(VOS({"1", "2", "3", "4"}), "1.2.3.*") );
+	ASSERT_TRUE( is_match(VOS({"1", "2", "3", "4"}), "1.2.3.4") );
+	ASSERT_TRUE( is_match(VOS({"1", "2", "3", "4"}), "1.02.003.04") );
+}
+
+TEST(is_match_tests, test2)
+{
+	ASSERT_FALSE( is_match(VOS({"1", "2", "3", "4"}), "*.*.*.0") );
+	ASSERT_FALSE( is_match(VOS({"1", "2", "3", "4"}), "0.*.*.*") );
+	ASSERT_FALSE( is_match(VOS({"1", "2", "3", "4"}), "1.0.*.*") );
+	ASSERT_FALSE( is_match(VOS({"1", "2", "3", "4"}), "1.2.0.*") );
+	ASSERT_FALSE( is_match(VOS({"1", "2", "3", "4"}), "1.2.3.0") );
+}
+
+std::vector<VOS> load_input(std::string filename)
+{
+	std::ifstream fin(filename);
+	std::vector<VOS> ip_pool;
+
+	for(std::string line; std::getline(fin, line);)
+	{
+		vec_of_strings v = split(line, '\t');
+		ip_pool.push_back(split(v.at(0), '.'));
+	}
+
+	fin.close();
+	return ip_pool;
+}
+
+std::vector<VOS> load_result(std::string filename)
+{
+	std::ifstream fin(filename);
+	std::vector<VOS> ip_pool;
+
+	for(std::string line; std::getline(fin, line);)
+	{
+		vec_of_strings v = split(line, '\t');
+		ip_pool.push_back(split(v.at(0), '.'));
+	}
+
+	fin.close();
+	return ip_pool;
+}
+
+TEST(filter_tests, test1)
+{
+	std::vector<VOS> ip_pool = load_input("ip_filter.tsv");
+}
+
